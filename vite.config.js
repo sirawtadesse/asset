f136aa0ed53fab0504d77b92abcfbc1ddf838754
Siraw Tadesse <128708396+sirawtadesse@users.noreply.github.com>
2025-09-04 13:09:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://sirawdev.com.et',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+};
