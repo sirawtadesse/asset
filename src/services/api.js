328b58b0e36ad72301f2ef4c468a2d987776941b
Siraw Tadesse <128708396+sirawtadesse@users.noreply.github.com>
2025-09-04 13:26:57 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://sirawdev.com.et/asset-management/api/';
+const API_BASE_URL = 'https://sirawdev.com.et/asset-management/api/login.php';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
